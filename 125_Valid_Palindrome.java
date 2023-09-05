6f30796e52237696c086a31c80e855951d58c0a9
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-09-05 23:52:28 +0530

    125_Valid_Palindrome.java

---
+++ 125_Valid_Palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+   public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
