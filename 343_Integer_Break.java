3ef31547ea1d26dcd3f044cc019058cc5a7dd2ba
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2023-10-06 16:50:14 +0530

    Problem of the day 06-10-2023

---
+++ 343_Integer_Break.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) return n - 1;
+        int quotient = n / 3, remainder = n % 3;
+        return remainder == 0 ? (int)Math.pow(3, quotient) : (remainder == 1 ? (int)Math.pow(3, quotient - 1) * 4 : (int)Math.pow(3, quotient) * 2);
+    }
+}
